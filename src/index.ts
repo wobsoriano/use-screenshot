e20f41b10e5eb986892f2c2da4ca9eaca32cd55c
Robert Soriano <sorianorobertc@gmail.com>
2022-06-18 18:16:20 -0700

    working composable

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-export const one = 1
-export const two = 2
+import { takeScreenshot as _takeScreenshot, checkIfBrowserSupported } from '@xata.io/screenshot'
+import { ref } from 'vue'
+
+export function useScreenshot() {
+  const screenshot = ref('')
+  const isSupported = checkIfBrowserSupported()
+
+  const capture = async () => {
+    if (checkIfBrowserSupported())
+      screenshot.value = await _takeScreenshot()
+  }
+
+  return {
+    isSupported,
+    capture,
+    screenshot,
+  }
+}
